dc5f45f645e7d14a59ef97b07ac5efbbb12d06bd
Penka <williams.penka@gmail.com>
2019-06-21 08:51:28 +0100

    make it better

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -102,6 +102,8 @@ public function makup($details){
     if (is_null($detail['stockFinal'])&&$detail['stock']!=null){
      $detail['stockFinal']=($detail['stock']-$detail['variante']-$detail['gratuite']);
     }
+    if($detail['stockFinal']<0)
+        $detail['stockFinal']=0;
   }
   return $details;
 }
